28bc1a314ed1cfb8f9c9726e7cf164fc050cbd46
Edson <edsonjnior@gmail.com>
2018-06-29 16:38:37 -0300

    Termino da implementação do código

--- PROVA_01/questao_01.py
+++ PROVA_01/questao_01.py
@@ -2,3 +2,16 @@
 
 def valida_angulo(angulo):
     return angulo >= 0 and angulo <= 360
+
+angulo = int(input('Qual o valor do angulo? '))
+if(valida_angulo(angulo)):
+    if angulo >= 0 and angulo <= 90:
+        print("PRIMEIRO")
+    elif angulo > 90 and angulo <= 180:
+        print("SEGUNDO")
+    elif angulo > 180 and angulo <= 270:
+        print("TERCEIRO")
+    elif angulo > 270 and angulo <= 360:
+        print("QUARTO")
+else:
+    print("O valor do ângulo informado é inválido.")
