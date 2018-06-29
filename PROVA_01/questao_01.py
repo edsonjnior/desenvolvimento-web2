5638059c6392e802e45b6012da69abc8b9ffb3be
Edson <edsonjnior@gmail.com>
2018-06-29 16:43:20 -0300

    Alteração no comentário inicial

--- PROVA_01/questao_01.py
+++ PROVA_01/questao_01.py
@@ -1,4 +1,4 @@
-# dado um determinado ângulo, determinar em qual quadrante esse angulo se encontra
+# dado um ângulo, determinar em qual quadrante esse angulo se encontra
 
 def valida_angulo(angulo):
     return angulo >= 0 and angulo <= 360
